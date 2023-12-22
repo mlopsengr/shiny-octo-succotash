aa1d05e6b7b34e619f57041939885d48c9344618
Tobi John <tobioluwafemi58@gmail.com>
2023-12-22 18:58:23 +0000

    Initial Commit

---
+++ Formula1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Formula1
+//
+//  Created by Oluwatobi John Oluwafemi on 22/12/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
