ebb5796160ef6abc784444e2a08281aeec939a2d
Tobi John <tobioluwafemi58@gmail.com>
2023-12-22 20:51:30 +0000

    Updates grid

--- Formula1/main.cpp
+++ Formula1/main.cpp
@@ -6,9 +6,28 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+// Constants
+const double dt = 0.01; // Time step
+const double dx = 0.1; // Spatial step
+const int nx = 50;
+const int ny = 50;
+
+// Function to initialize the grid
+void initialize(std::vector<std::vector<double>>& grid) {
+    std::vector<std::vector<double>> newGrid(nx, std::vector<double>(ny, 0.0));
+    
+    for (int i = 1; i < nx - 1; ++i) {
+        for (int j = 1; j < ny - 1; ++j) {
+            // Simple advection equation
+            newGrid[i][j] = grid[i][j] - (dt / dx) * (grid[i][j] - grid[i - 1][j]);
+        }
+    }
+    
+    // Update the grid
+    grid = newGrid;
 }
+
+// Function to print grid
+
